08730cf4dc091c6133971bba3d1f25cff004a49c
PabloRMCarneiro <pablormc3dd3@gmail.com>
2022-06-20 17:53:31 -0300

    first commit

---
+++ orders.js
@@ -0,0 +1,45 @@
+const Orders =[
+    {
+        productName: 'Foldable Mini Drone',
+        produtNumber: '85631',
+        paymentSatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'LARVENDER KF102 Drone',
+        produtNumber: '36378',
+        paymentSatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ruku Fu Pro Drone',
+        produtNumber: '49347',
+        paymentSatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Drone with Camera Drone',
+        produtNumber: '96996',
+        paymentSatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'GPS 4k Drone',
+        produtNumber: '22821',
+        paymentSatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'DJI Air 2s',
+        produtNumber: '81475',
+        paymentSatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Lozenge Drone',
+        produtNumber: '00482',
+        paymentSatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    
+]
